b22a584e3262105cc7b7da96b74bf6f66e11e531
andrew <and123rex@gmail.com>
2016-06-05 20:43:56 -0600

    removed comments

--- whereAmI/src/game/TileControl.java
+++ whereAmI/src/game/TileControl.java
@@ -9,21 +9,5 @@ public class TileControl {
 		int y = tile.getY();
 		tile.setX(x += Dx);
 		tile.setY(y += Dy);
-		// int[] a = new int[2];
-		// int[] b = new int[2];
-		// int[] c = new int[2];
-		// int[] d = new int[2];
-		// a[0] = (x - (tileSize));
-		// a[1] = (y - (tileSize));
-		// b[0] = (x - (tileSize));
-		// b[1] = (y + (tileSize));
-		// c[0] = (x + (tileSize));
-		// c[1] = (y + (tileSize));
-		// d[0] = (x + (tileSize));
-		// d[1] = (y - (tileSize));
-		// tile.setA(a);
-		// tile.setB(b);
-		// tile.setC(c);
-		// tile.setD(d);
 	}
 }
